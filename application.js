4e094d3f5d7310df0792b42e94e345ca2c4e77a6
danto1020 <magotaro.n.1020@gmail.com>
2021-02-23 14:33:35 +0900

    色々な変更

--- application.js
+++ application.js
@@ -1,8 +1,43 @@
+var mySwiper = new Swiper ('.swiper-container', {
+	loop: true,
+	slidesPerView: 2,
+	spaceBetween: 10,
+	centeredSlides : true,
+	pagination: '.swiper-pagination',
+	nextButton: '.swiper-button-next',
+	prevButton: '.swiper-button-prev',
+	breakpoints: {
+	  767: {
+		slidesPerView: 1,
+		spaceBetween: 0
+	  }
+	}
+  })
+
+  let swipeOptionTop = {
+	loop: true,
+	effect: 'slide',
+	speed: 2000,
+	autoplay: {
+	  delay: 30000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	}
+  }
+  var mySwiperTop = new Swiper('.swiper-container-top', swipeOptionTop);
+
+  mySwiperTop.on('init', function () {
+	console.log('initialized');
+  });
+
 $(function () {
     var pagetop = $('#page_top');
     pagetop.hide();
     $(window).scroll(function () {
-//150の数字のところでどこでフェードアウトさせるか調整
+		//150の数字のところでどこでフェードアウトさせるか調整
 	    if ($(this).scrollTop() > 150) {
 	      pagetop.fadeIn();
 	    } else {
@@ -10,11 +45,11 @@ $(function () {
 	    }
     });
 
-pagetop.click(function () {
-	$('body,html').animate({
-	    scrollTop: 0
-//500の数字のところでスピード調整
-	}, 500);
-	  return false;
-  });
+	pagetop.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+	//500の数字のところでスピード調整
+		}, 500);
+		return false;
+	});
 });
